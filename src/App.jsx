03e56aa967029a1de8bd74080221e5b5270f4d60
dhanush-ait <dhanushprabaharan10@gmail.com>
2025-10-17 11:06:42 +0530

    17th oct app and tasklist

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,24 @@ function App() {
     setTasks([...tasks,task]);
   }
 
+  const updateTask = (updatedTask, index) => {
+    const newtask = [...tasks];
+    newtask[index] = updatedTask;
+    setTasks(newtask);
+  }
+
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i != index));
+  }
+
   return(
     <div>
       <h1>Task Mate</h1>
       <p>Our friendly task manager</p>
       <Taskform addTask = {addTask}/>
-      <TaskList />
+      <TaskList tasks = {tasks}
+      updateTask = {updateTask} 
+      deleteTask = {deleteTask}/>
       <Progresstracker />
       <button>Clear all tasks</button>
     </div>
